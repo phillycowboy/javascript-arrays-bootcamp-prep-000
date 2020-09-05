6b96fa1b65911f2d6370dcd7c2cd0ec4bb3f6a75
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-05 01:25:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -44,7 +44,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 function removeElementFromEndOfArray(array){
 
-  array.slice( array.length - 1);
+  array.slice(0);
   return array;
 }
 
