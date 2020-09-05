51901dddae58e2c1e403b66c675fe1fd012ef1f2
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-05 01:10:51 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+
+function addElementToBeginningOfArray(array, element){
+  var finalArray=[element,...array];
+  return finalArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var finalArray=[...array,element];
+  return finalArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var n= [1,2,3]
+  array=array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  var n=[1,2,3];
+  array.slice(3);
+  return array;
+}
+
